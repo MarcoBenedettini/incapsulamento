50c05669409ff28a7480b426ea209f5141d3a1df
MarcoBenedettini <marco.bene07@gmail.com>
2025-01-31 10:38:19 +0100

    Add files via upload

---
+++ BenedettiniTartamella/main.cpp
@@ -0,0 +1,22 @@
+/*888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+inrandazzamento bio ulcera messaggio in file c++ preleva; lo converte in binario, lo mette in una strucut del livello 3 poi lo mette in una struct di ethernet convertire in binario e scrivi su un'altro film tutto il pacchetto
+
+888888888888888888888888888888888888888888888888888888888888888888888888888888*/
+
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void leggi();
+void scrivi();
+string convtoS(char c);
+
+int main()
+{
+	leggi();
+	convtoS(64);
+    return 0;
+}
